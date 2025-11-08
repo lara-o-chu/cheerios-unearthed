ec85662c649f7e5809aef188f2b7f738526e257a
lara-o-chu <140978887+lara-o-chu@users.noreply.github.com>
2025-11-08 09:03:43 -0800

    CheerioBot: Add optional wait parameter to drive and turn

--- cheerio_bot.py
+++ cheerio_bot.py
@@ -2,6 +2,7 @@ from pybricks.pupdevices import Motor,ColorSensor
 from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from pybricks.tools import StopWatch
 
 
 class Cheerio:
@@ -17,13 +18,17 @@ class Cheerio:
 
     cheerio_drive.use_gyro(True)
 
-    def drive_straight(self, speed, distance):
-        self.cheerio_drive.settings(straight_speed=speed)
-        self.cheerio_drive.straight(distance=distance)
+    def is_stalled(self):
+        return self.cheerio_drive.stalled()
 
-    def turn(self, speed, degrees):
+    def drive_straight(self, speed, distance, wait=True):
+        self.cheerio_drive.settings(straight_speed=speed)
+        self.cheerio_drive.straight(distance=distance,wait=wait)
+
+    def turn(self, speed, degrees, wait=True):
         self.cheerio_drive.settings(turn_rate=speed)
-        self.cheerio_drive.turn(angle=degrees)
+        self.cheerio_drive.turn(angle=degrees, wait=wait)
+
     
     def clean_dot_wheels_dot_com(self):
         self.drive_straight(speed=500, distance=10000)
