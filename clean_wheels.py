bbff2bd1bf83b45db95e2d1438d2fdec4f7bc2ff
lara-o-chu <140978887+lara-o-chu@users.noreply.github.com>
2025-11-10 15:13:47 -0800

    Added clean_wheels to menu

---
+++ clean_wheels.py
@@ -0,0 +1,9 @@
+from cheerio_bot import Cheerio
+from pybricks.tools import wait
+from pybricks.tools import StopWatch
+
+def run_clean_wheels():
+    bot = Cheerio()
+    bot.clean_dot_wheels_dot_com()
+
+
