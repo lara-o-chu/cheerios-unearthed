35e5d0292c97e90791298de53c1e7e3a68e0ff9d
lara-o-chu <140978887+lara-o-chu@users.noreply.github.com>
2025-11-11 09:12:22 -0800

    Added Angler Arm to Ship Mission

--- pirate_boat.py
+++ pirate_boat.py
@@ -5,24 +5,26 @@ def run_pirate_boat():
 
     # Start in red home area 
     # Drive forward until pushing against boat mission
-    bot.drive_straight(speed=400, distance=580)
+    bot.drive_straight(speed=300, distance=580)
 
     
     # Lower arm
-    bot.left_attachment_motor.run_angle(speed=150, rotation_angle=-2160)
-    bot.drive_straight(speed=200, distance=-20)
-    bot.left_attachment_motor.run_angle(speed=150, rotation_angle=360)
-    '''# Change to gear spin side of gear 
-    bot.right_attachment_motor.run_angle(speed=100, rotation_angle=-180)
-    # Spin gears
-    bot.left_attachment_motor.run_angle(speed=200, rotaion_angle=500)
-    #change to lifty thingy gear setting
-    bot.right_attachment_motor(speed=100, rotation_angle=-180)
-    # Raise arm
-    bot.left_attachment_motor(speed=150, distance=-720)
+    bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=-2160)
+    bot.drive_straight(speed=100, distance=-20)
+    bot.drive_straight(speed=100, distance=30)
+
+    # Change to gear spin side of gear 
+    bot.right_attachment_motor.run_angle(speed=100, rotation_angle=-200)
+    #spin thingy
+    bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=-4000)
+    #Switch setting
+    bot.right_attachment_motor.run_angle(speed=100,rotation_angle=-220)
+    bot.drive_straight(speed=200, distance=-75)
+    #lift arm
+    bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=2900)
     # Back up to home (Hopefully pull sand)
-    bot.drive_straight(speed=400,distance= -447)
-    '''
+    bot.drive_straight(speed=400,distance=-750)
+    
 
 
 
