391daf6465faba207674afb79b67b356a43a2388
lara-o-chu <140978887+lara-o-chu@users.noreply.github.com>
2025-11-14 22:23:59 -0800

    Boat slight speedup

--- pirate_boat.py
+++ pirate_boat.py
@@ -6,8 +6,8 @@ def run_pirate_boat():
 
     # Start in red home area 
     # Drive forward until pushing against boat mission
-    bot.drive_straight(speed=300, distance=580)
-
+    bot.drive_straight(speed=300, distance=580, wait=False)
+    wait(1000)  
     
     # Lower arm
     bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=-2160)
@@ -16,15 +16,18 @@ def run_pirate_boat():
 
     # Change to gear spin side of gear 
     bot.right_attachment_motor.run_angle(speed=100, rotation_angle=-200)
-    #spin thingy
+    
+    # Angler Artifacts
     bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=-4000)
+
     #Switch setting
     bot.right_attachment_motor.run_angle(speed=100,rotation_angle=-220)
     bot.drive_straight(speed=200, distance=-75)
-    #lift arm
-    #bot.left_attachment_motor.run_angle(speed=2000, rotation_angle=2900)
+
+    # Start lifting arm and wait long enough for it to be higher than mission
     bot.left_attachment_motor.run(speed=2000)
     wait(1500)
+    
     # Back up to home (Hopefully pull sand)
     bot.drive_straight(speed=400,distance=-750)
     bot.left_attachment_motor.stop()
